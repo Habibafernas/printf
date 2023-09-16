ae9398e5cab5335d0b0a4ea7bb1ff48a5919c019
Moaaz Abdullah <138708259+moaaz-abdullah@users.noreply.github.com>
2023-09-16 17:26:37 +0300

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -1,93 +1,47 @@
 #include "main.h"
 #include <stdarg.h>
 /**
- * _printf - Produces output according to a format.
- * @format: Format string containing the characters and specifiers.
- *
- * Return: The number of characters printed (excluding the null byte).
+ * _printf - produces output according to a format
+ * @format: format string containing the characters and the specifiers
+ * Description: this function will call the get_print() function that will
+ * determine which printing function to call depending on the conversion
+ * specifiers contained into fmt
+ * Return: length of the formatted output string
  */
 int _printf(const char *format, ...)
 {
-	va_list args;
-	int cprint = 0;
+	int (*pfunc)(va_list, flags_t *);
+	const char *p;
+	va_list arguments;
+	flags_t flags = {0, 0, 0};
 
-	if (format == NULL)
+	register int count = 0;
+
+	va_start(arguments, format);
+	if (!format || (format[0] == '%' && !format[1]))
 		return (-1);
-
-	va_start(args, format);
-	cprint = process_format(format, args);
-	va_end(args);
-
-	return (cprint);
-}
-
-/**
- * process_format - Processes format specifiers in the format string.
- * @format: Format string containing the characters and specifiers.
- * @args: va_list containing the arguments for format specifiers.
- *
- * Return: The number of characters printed.
- */
-int process_format(const char *format, va_list args)
-{
-	int cprint = 0;
-
-	while (*format)
+	if (format[0] == '%' && format[1] == ' ' && !format[2])
+		return (-1);
+	for (p = format; *p; p++)
 	{
-		if (*format == '%')
+		if (*p == '%')
 		{
-			format++;
-			if (*format == '\0')
-				break;
-
-			int chars_printed = process_specifier(*format, args);
-			if (chars_printed == -1)
-				return (-1);
-
-			cprint += chars_printed;
-			format++;
-		}
-		else
-		{
-			_putchar(*format);
-			cprint++;
-			format++;
-		}
+			p++;
+			if (*p == '%')
+			{
+				count += _putchar('%');
+				continue;
+			}
+			while (get_flag(*p, &flags))
+				p++;
+			pfunc = get_print(*p);
+			count += (pfunc)
+				? pfunc(arguments, &flags)
+				: _printf("%%%c", *p);
+		} else
+			count += _putchar(*p);
 	}
-
-	return (cprint);
-}
-
-/**
- * process_specifier - Processes a format specifier and prints its value.
- * @specifier: The format specifier character.
- * @args: va_list containing the arguments for format specifiers.
- *
- * Return: The number of characters printed for this specifier.
- */
-int process_specifier(char specifier, va_list args)
-{
-	int cprint = 0;
-
-	switch (specifier)
-	{
-		case 'c':
-			cprint += print_char(va_arg(args, int));
-			break;
-		case 's':
-			cprint += print_string(va_arg(args, char *));
-			break;
-		case 'i':
-		case 'd':
-			cprint += print_int(va_arg(args, int));
-			break;
-		/* Add more format specifiers as needed */
-		default:
-			_putchar('%');
-			_putchar(specifier);
-			cprint += 2;
-			break;
-	}
-
-	return (cprint);
+	_putchar(-1);
+	va_end(arguments);
+	return (count);
 }
