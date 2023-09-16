9b6ea690f6fda3b1fad02d234cffc41f878a9909
Habibafernas <habibafernas@gmail.com>
2023-09-16 07:08:32 -0700

    addint printf file and strlen file

---
+++ _printf.c
@@ -0,0 +1,13 @@
+#include "main.h"
+ /**
+  * _printf - function that prints output
+  * @format : const char that is used to select input
+  * Return : length of the string
+  */
+int _printf(const char *format, ...)
+{
+	int i = 0;
+
+	char *str, 
+        
+}
---
+++ printf_str.c
@@ -0,0 +1,7 @@
+#include "main.h"
+/**
+ * _strlen - returns the lenght of the string
+ * @s : string to calculate its length
+ * Return : length of the string
+ */
+int _Strlen()
