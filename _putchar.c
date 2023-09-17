5f1c283c80b93774eb59188a4ec95a84135e37a0
Habibafernas <habibafernas@gmail.com>
2023-09-17 03:02:55 -0700

    modify putchar

--- _putchar.c
+++ _putchar.c
@@ -22,16 +22,3 @@ int buffer(char c)
 
         return (1);
 }
-
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned.
- */
-int _putchar(char c)
-{
-	return (buffer(c));
-}
-
