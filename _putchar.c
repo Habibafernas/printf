c35294b3d843c0f13eef808b7966b6f3085669cb
Habibafernas <habibafernas@gmail.com>
2023-09-17 02:47:20 -0700

    modify buffer

--- _putchar.c
+++ _putchar.c
@@ -1,4 +1,28 @@
 #include "main.h"
+
+/**
+ * buffer - Save the character in a buffer
+ * @c: Character
+ *
+ * Return: 1
+ **/
+int buffer(char c)
+{
+        static char buffering[1024];
+        static int i;
+
+        if (c == -1 || i == 1024)
+        {
+                write(1, buffering, i);
+                i = 0;
+        }
+
+        if (c != -1)
+                buffering[i++] = c;
+
+        return (1);
+}
+
 /**
  * _putchar - writes the character c to stdout
  * @c: The character to print
@@ -11,25 +35,3 @@ int _putchar(char c)
 	return (buffer(c));
 }
 
-/**
- * buffer - Save the character in a buffer
- * @c: Character
- *
- * Return: 1
- **/
-int buffer(char c)
-{
-	static char buffering[1024];
-	static int i;
-
-	if (c == -1 || i == 1024)
-	{
-		write(1, buffering, i);
-		i = 0;
-	}
-
-	if (c != -1)
-		buffering[i++] = c;
-
-	return (1);
-}
