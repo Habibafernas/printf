db69a875b35436520316a371fdff270d3589a241
Habibafernas <habibafernas@gmail.com>
2023-09-17 03:00:10 -0700

    modify putchar

--- _put.c
+++ _put.c
@@ -1,4 +1,26 @@
 #include "main.h"
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+        static int i;
+        static char buf[OUTPUT_BUF_SIZE];
+
+        if (c == BUF_FLUSH || i >= OUTPUT_BUF_SIZE)
+        {
+                write(1, buf, i);
+                i = 0;
+        }
+        if (c != BUF_FLUSH)
+                buf[i++] = c;
+        return (1);
+}
+
 
 /**
  * _puts - prints a string with newline
@@ -14,26 +36,3 @@ int _puts(char *str)
 		_putchar(*str++);
 	return (str - a);
 }
-
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
-{
-	static int i;
-	static char buf[OUTPUT_BUF_SIZE];
-
-	if (c == BUF_FLUSH || i >= OUTPUT_BUF_SIZE)
-	{
-		write(1, buf, i);
-		i = 0;
-	}
-	if (c != BUF_FLUSH)
-		buf[i++] = c;
-	return (1);
-}
-
