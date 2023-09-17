edf6d1f1c066d7345ff1ef19f45c900b772a08f1
moaaz-abdullah <moaaz-abdullah>
2023-09-17 09:44:58 +0300

    Done by me

--- _printf.c
+++ _printf.c
@@ -1,17 +1,46 @@
-<<<<<<< HEAD
 #include "main.h"
- /**
-  * _printf - function that prints output
-  * @format : const char that is used to select input
-  * Return : length of the string
-  */
+/**
+ * _printf - produces output according to a format
+ * @format: format string containing the characters and the specifiers
+ * Description: this function will call the get_print() function that will
+ * determine which printing function to call depending on the conversion
+ * specifiers contained into fmt
+ * Return: length of the formatted output string
+ */
 int _printf(const char *format, ...)
 {
-	int i = 0;
+	int (*pfunc)(va_list, flags_t *);
+	const char *p;
+	va_list arguments;
+	flags_t flags = {0, 0, 0};
 
-	char *str, 
-        
+	register int count = 0;
+
+	va_start(arguments, format);
+	if (!format || (format[0] == '%' && !format[1]))
+		return (-1);
+	if (format[0] == '%' && format[1] == ' ' && !format[2])
+		return (-1);
+	for (p = format; *p; p++)
+	{
+		if (*p == '%')
+		{
+			p++;
+			if (*p == '%')
+			{
+				count += _putchar('%');
+				continue;
+			}
+			while (get_flag(*p, &flags))
+				p++;
+			pfunc = get_print(*p);
+			count += (pfunc)
+				? pfunc(arguments, &flags)
+				: _printf("%%%c", *p);
+		} else
+			count += _putchar(*p);
+	}
+	_putchar(-1);
+	va_end(arguments);
+	return (count);
 }
-=======
-inc
->>>>>>> eecd5c93f17480f6e9f9c633475b6df8fa06cba6
